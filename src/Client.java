86e955da099692b4d470b9aac24cefbfef22bad1
Jcastilloq1 <j.castilloq@uniandes.edu.co>
2024-05-03 19:29:19 -0500

    cambio

--- src/Client.java
+++ src/Client.java
@@ -45,5 +45,17 @@ public class Client {
         }
     }
 
+    public static void imprimirHexa(byte[] byteArray){
+        String out = "";
+        for (int i = 0; i < byteArray.length; i++){
+            if((byteArray[i] & 0xff) <= 0xf){
+                out = "0";
+            }
+            out += Integer.toHexString(byteArray[i] & 0xff).toLowerCase();
+
+        }
+        System.out.println(out);
+    }
+
 }
 
